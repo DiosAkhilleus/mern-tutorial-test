8b52a4102e3110404138c39703e00a4dc00316db
DiosAkhilleus <abertin1809@gmail.com>
2021-07-15 16:11:13 -0500

    reset files

---
+++ client/src/components/CreateStudent.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const CreateStudent = () => {
+  return (
+    <div>
+      <p>React CreateStudent Component</p>
+    </div>
+  )
+}
+
+export default CreateStudent
---
+++ client/src/components/StudentList.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const StudentList = () => {
+  return (
+    <div>
+      <p>React StudentList Component</p>
+    </div>
+  )
+}
+
+export default StudentList

887598cefc479472c9776e6ae99b9d3f479a0bd9
DiosAkhilleus <abertin1809@gmail.com>
2021-07-15 21:51:40 -0500

    basic CRUD functionality added

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,95 @@
 import 'bootstrap/dist/css/bootstrap.css';
-
+import './App.css';
+import { useState, useEffect } from 'react';
+import Axios from 'axios';
 
 function App() {
+  const [foodName, setFoodName] = useState('');
+  const [daysSince, setDaysSince] = useState(0);
+  const [newFoodName, setNewFoodName] = useState('');
+  const [update, setUpdate] = useState(false);
+  const [foodList, setFoodList] = useState([]);
+
+  useEffect(() => {
+    Axios.get('http://localhost:3001/read').then((response) => {
+      setFoodList(response.data);
+    });
+  }, [update]);
+
+  const addToList = () => {
+    Axios.post('http://localhost:3001/insert', {
+      foodName: foodName,
+      daysSinceEaten: daysSince,
+    }).then(setUpdate(!update));
+  };
+
+  const updateFood = (id) => {
+    // console.log(id, newFoodName);
+    Axios.put('http://localhost:3001/update', {
+      id: id,
+      newFoodName: newFoodName,
+    }).then(setUpdate(!update));
+  };
+
+  const removeFood = (id) => {
+    Axios.delete(`http://localhost:3001/remove/${id}`).then(setUpdate(!update));
+  };
+
   return (
     <div className="App">
+      <h1>CRUD App with MERN</h1>
+      <label htmlFor="">Food Name: </label>
+      <input
+        type="text"
+        onChange={(e) => {
+          setFoodName(e.target.value);
+        }}
+      />
+      <label htmlFor="">Days Since Eaten: </label>
+      <input
+        type="number"
+        onChange={(e) => {
+          setDaysSince(e.target.value);
+        }}
+      />
+      <button onClick={addToList}>Add To List</button>
+      <h2>Food List</h2>
+      <hr
+        style={{
+          backgroundColor: 'black',
+          color: 'black',
+          height: '2px',
+          width: '100%',
+        }}
+      />
+      <div>
+      {foodList.map((element, index) => {
+        return (
+          <div className="food" key={Math.random()}>
+            <h2>{element.foodName}</h2>
+            <h3>Days Since Eaten: {element.daysSinceEaten}</h3>
+            <input
+              type="text"
+              placeholder="New Food Name..."
+              onChange={(e) => {
+                setNewFoodName(e.target.value);
+              }}
+            />
+            <button
+              onClick={() => {
+                updateFood(element._id);
+              }}
+            >
+              Update Name
+            </button>
+            <button onClick={() => {
+              removeFood(element._id);
+            }}>Delete Food</button>
+          </div>
+        );
+      })}
+      </div>
+      
     </div>
   );
 }
--- server/index.js
+++ server/index.js
@@ -1,11 +1,11 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const cors = require('cors');
 const app = express();
-
 const FoodModel = require('./models/Food');
 
 app.use(express.json());
-
+app.use(cors());
 mongoose.connect(
   'mongodb+srv://diosakhilleus:mern@cluster0.zgiaf.mongodb.net/food?retryWrites=true&w=majority',
   {
@@ -14,17 +14,50 @@ mongoose.connect(
   }
 );
 
-app.get('/', async (req, res) => {
+app.post('/insert', async (req, res) => {
+  const foodName = req.body.foodName;
+  const daysSinceEaten = req.body.daysSinceEaten;
   const food = new FoodModel({
-    foodName: 'Cheebs',
-    daysSinceEaten: 6,
+    foodName: foodName,
+    daysSinceEaten: daysSinceEaten,
   });
   try {
     await food.save();
   } catch (err) {
     console.error(err);
   }
-  res.end('entry added');
+  res.writeHead(200, { 'Content-Type': 'text/html' });
+  res.end('posted');
+});
+
+app.get('/read', async (req, res) => {
+  FoodModel.find({}, (err, result) => {
+    if (err) {
+      res.send(err);
+    } 
+    res.send(result);
+  });
+});
+
+app.put('/update', async (req, res) => {
+  const newFoodName = req.body.newFoodName;
+  const id = req.body.id;
+  try {
+    await FoodModel.findById(id, (err, updatedFood) => {
+      updatedFood.foodName = newFoodName;
+      updatedFood.save();
+      res.send('update');
+    });
+  } catch (err) {
+    console.error(err);
+  }
+});
+
+app.delete('/remove/:id', async (req, res) => {
+  const id = req.params.id;
+  await FoodModel.findByIdAndDelete(id).exec();
+  console.log(`deleting entry ${id}`);
+  res.send('del');
 });
 
 app.listen(3001, () => {
